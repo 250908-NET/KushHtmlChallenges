4d86ec3022b39033e03feed8a72988afa62b1650
Kush Gandhi <Kushgandhi2099@gmail.com>
2025-10-12 11:52:35 -0500

    complete step 1

---
+++ react_to_do/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>My Task List</h1>
+      <div>
+        <p>Learn React</p>
+        <p>Build a project</p>
+        <p>Deploy to production</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
