748b868fd5bbe8e490ad2e6e69f436ece440a35d
Kush Gandhi <Kushgandhi2099@gmail.com>
2025-10-12 15:28:56 -0500

    complete step 3

--- react_to_do/src/App.jsx
+++ react_to_do/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import './App.css';
+
 function TaskItem(props) {
   return (
     <div style={{
@@ -18,9 +21,52 @@ function App() {
     { id: 3, text: 'Deploy to production' }
   ];
 
+  //State: current value of input field
+  const [newTaskText, setNewTaskText] = useState('');
+
+  //Handler to add a new task
+  const addTask = () => {
+    if (newTaskText.trim() === '') return; //Don't add empty tasks
+
+    const newTask = {
+      id: Date.now(), //Simple way to generate uniqie IDs
+      text: newTaskText
+    };
+
+    setTasks([...tasks, newTask]) //add new task to array
+    setNewTaskText(''); //clear the input
+  }
   return (
     <div className="App">
       <h1>My Task List</h1>
+      
+      {/* Input Section */}
+      <div style={{ marginBottom: '20px'}}>
+        <input
+        type="text"
+        value={newTaskText}
+        onChange={(e) => setNewTaskText(e.target.value)}
+        placeholder="Enter a new Task"
+        style={{ padding: '8px', width: '70%', fontSize: '16px'}}
+      />
+      <button
+        onClick={addTask}
+        style={{
+          padding: '8px 16px',
+            marginLeft: '10px',
+            fontSize: '16px',
+            backgroundColor: '#4CAF50',
+            color: 'white',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer'
+        }}
+        >
+          Add Task
+        </button>
+      </div>
+      
+      {/* Task list */}
       <div>
         {tasks.map(task => (
           <TaskItem key={task.id} text={task.text} />
